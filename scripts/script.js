676e1dcc48b295f42275f66dd3683b5d4366dc50
Alexander Koik-Cestone <akoikcestone@gmail.com>
2019-12-10 08:06:23 -0500

    First release-ready version

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,50 @@ window.onload = function() {
 
 class Scroller {
     constructor() {
-        var conts = document.querySelectorAll('.container > div');
+        this.switchToBottom = false;
+        let _self = this;
+        
+        let conts = document.querySelectorAll('.container');
         
         conts.forEach(function(cont) {
-            cont.onclick = function() {
-                alert('clicked');
+            cont.onclick = function(e) {
+                let nextTop = 0;
+                
+                try {
+                    nextTop = this.nextSibling.nextSibling.getBoundingClientRect().top;
+                } catch(e) {
+                    nextTop = (this.scrollTop + this.clientHeight >= this.scrollHeight)? -document.querySelector('.outer-container').scrollHeight : this.getBoundingClientRect().top;
+                }
+                
+                document.querySelector('.outer-container').scrollBy({top: nextTop, behavior: 'smooth'});
+
             }
         });
-                      
+        
+        let links = document.querySelectorAll('a');
+        links.forEach(function(link) {
+            link.onclick = function(e) {
+                e.stopPropagation();
+            }
+        });
+        
+        document.querySelector('.scroll-button img').onclick = function(e) {
+            //alert('clicked');
+        }
+        
+        document.querySelector('.outer-container').onscroll = function(e) {
+            // if at bottom, reverse the pip.
+            if (this.scrollTop + this.clientHeight >= this.scrollHeight) {
+                document.getElementById('img-scroller').style.animation = 'scrollButtonAnim 1s infinite alternate, rotateScroll 1s forwards';
+                this.switchToBottom = true;
+            } else {
+                if (this.switchToBottom) {
+                    document.getElementById('img-scroller').style.animation = 'scrollButtonAnim 1s infinite alternate, rotateScrollRev 1s forwards';
+                }
+                
+                this.switchToBottom = false;
+            }
+        }
                       
     }
 }
--- scripts/smooth-scroll.js
+++ scripts/smooth-scroll.js
@@ -1,23 +1,28 @@
 // Vanilla JavaScript Scroll to Anchor
 // @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
 
-(function() {
+//(function() {
+window.onload = function() {
 	scrollTo();
-})();
+}
+//})();
 
 function scrollTo() {
 	const links = document.querySelectorAll('.scroll');
 	links.forEach(each => (each.onclick = scrollAnchors));
+	//links.forEach(each => (each.onclick = function() {alert('hello');}));
 }
 
 function scrollAnchors(e, respond = null) {
 	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
 	e.preventDefault();
-	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-	const targetAnchor = document.querySelector(targetID);
+//	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	var targetID = (respond) ? respond.getAttribute('targetid') : this.getAttribute('targetid');
+	const targetAnchor = document.querySelector('#'+targetID);
 	if (!targetAnchor) return;
 	const originalTop = distanceToTop(targetAnchor);
-	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+//	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	document.querySelector('.outer-container').scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
 	const checkIfDone = setInterval(function() {
 		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
 		if (distanceToTop(targetAnchor) === 0 || atBottom) {
