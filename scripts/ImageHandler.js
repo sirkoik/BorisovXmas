20f64b91a2fe81b15917f06e67bbb54753a20f6a
Alexander Koik-Cestone <akoikcestone@gmail.com>
2021-12-01 17:53:19 -0500

    Basic image loading progress bar

--- scripts/ImageHandler.js
+++ scripts/ImageHandler.js
@@ -1,17 +1,42 @@
 export class ImageHandler {
   constructor() {}
 
-  load(url) {
-    return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.onload = () => {
-        resolve(url);
-      };
-      img.onerror = () => {
-        reject(url);
-      };
-      img.src = url;
-    });
+  // image loader with ability to measure download progress
+  // https://javascript.info/fetch-progress
+  async load(url) {
+    const response = await fetch(url);
+    const reader = response.body.getReader();
+
+    const length = +response.headers.get('Content-Length');
+    let loaded = 0;
+
+    const loadingBar = document.getElementById('loading-bar');
+
+    while (true) {
+      const { done, value } = await reader.read();
+
+      if (done) {
+        break;
+      }
+
+      loaded += value.length;
+      const loadPercent = (100 * loaded) / length;
+      loadingBar.style.width = `${loadPercent}%`;
+
+      // console.log(
+      //   'Bytes:',
+      //   value.length,
+      //   loaded,
+      //   length,
+      //   (100 * loaded) / length
+      // );
+    }
+
+    loadingBar.style.animation = 'hidebar 2s forwards';
+
+    // setTimeout(() => {
+    //   loadingBar.style.animation = 'hidebar 2s forwards';
+    // }, 2000);
   }
 
   replaceImg(url) {
