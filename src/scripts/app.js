dde48d3726062dc7e103f74c6779004609c6ccaa
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-02-02 11:31:30 -0500

    basic webpack bundle

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,6 @@
+import '../styles.css';
+import '../../assets/images/Arrow-down-white-2.svg';
+import '../../assets/videos/STSCI-H-v1953a-f-1148x952.mp4';
 import { Scroller } from './Scroller.js';
 
 const s = new Scroller();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/scripts/app.js',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|mp4)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
 };
