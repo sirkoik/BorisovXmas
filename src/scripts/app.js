739f13b469b5817244f1a2cb0285609a97686276
Alexander Koik-Cestone <akoikcestone@gmail.com>
2022-02-04 05:38:51 -0500

    -dist, +webpack-dev-server, fix video src

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,8 +2,13 @@ import '../styles.css';
 // import '../../assets/images/Arrow-down-white-2.svg';
 import '../../assets/videos/STSCI-H-v1953a-f-1148x952.mp4';
 import { Scroller } from './Scroller.js';
+import bgVideoFile from '../../assets/videos/STSCI-H-v1953a-f-1148x952.mp4';
 import internalScrollerImage from '../../assets/images/Arrow-down-white-2.svg';
 
 const s = new Scroller();
+
+const bgVideo = document.getElementById('video-source');
+bgVideo.src = bgVideoFile;
+
 const imgScroller = document.getElementById('img-scroller');
 imgScroller.src = internalScrollerImage;
